3cdeb7f9148af53995a14acc55a0521cccfa44c1
Matias De Martino <matias.demartino@botmaker.io>
2024-02-04 16:47:16 -0300

    initial config

---
+++ bci-global-logic/src/main/java/com/globallogic/bcigloballogic/BciGlobalLogicApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic.bcigloballogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciGlobalLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BciGlobalLogicApplication.class, args);
+	}
+
+}
